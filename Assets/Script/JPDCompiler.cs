363068bc9a9b218f2d21176718e29837e5d38434
JinHur <jinh2352@gmail.com>
2024-08-25 01:11:49 +0900

    MMO_Fighter_Client 대상으로 C# RPC 코드 컴파일 구현 중..

--- Assets/Script/JPDCompiler.cs
+++ Assets/Script/JPDCompiler.cs
@@ -25,10 +25,10 @@ public class JPDCompiler : MonoBehaviour
         {
             go = new GameObject { name = "@JPDCompiler" };
             go.AddComponent<JPDCompiler>();
-        }
 
-        DontDestroyOnLoad(go);
-        s_Instance = go.GetComponent<JPDCompiler>();
+            DontDestroyOnLoad(go);
+            s_Instance = go.GetComponent<JPDCompiler>();
+        }
     }
 
     private int m_CompileMode = 0;
@@ -447,7 +447,611 @@ public class JPDCompiler : MonoBehaviour
 
     private bool Compile_RPC_CLIENT()
     {
+        MakeComm_Client();
+        MakeProxy_Client(m_JPD.JPD);
+        MakeStub_Client(m_JPD.JPD);
+
         return true;
+    }
+
+    private void MakeStub_Client(List<JPD_NAMESPACE> jpdNamespaces)
+    {
+        string stubPath = m_JPD.CLIENT_OUTPUT_DIR + "\\Stub.cs";
+        string StubContent = $@"
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Stub : MonoBehaviour
+{{
+    public Dictionary<int, Action<byte[]>> methods = new Dictionary<int, Action<byte[]>>();
+}}
+
+";
+    
+        List<string> stubClassDefs = new List<string>();    
+        foreach(var jpdNamespace in jpdNamespaces)
+        {
+            string stubClass = MakeClientStubClass(jpdNamespace);
+        }
+    
+        using(FileStream fs = OpenFile(stubPath))
+        {
+            using(StreamWriter sw = new StreamWriter(fs))
+            {
+                sw.Write(StubContent);
+
+                foreach(var stubClassDef in stubClassDefs)
+                {
+                    sw.Write(stubClassDef);
+                }
+            }
+        }
+    }
+
+    private string MakeClientStubClass(JPD_NAMESPACE jpdNamespace)
+    {
+        string ret = $@"
+public abstract class Stub_{jpdNamespace.Namespace} : Stub
+{{
+    public void Init() 
+    {{
+";
+
+        return ret;
+    }
+
+    private void MakeComm_Client()
+    {
+        string commRpcPath = m_JPD.CLIENT_OUTPUT_DIR + "\\RPC.cs";
+        string commNetworkPath = m_JPD.CLIENT_OUTPUT_DIR + "\\NetworkManager.cs";
+
+        string TypeOfHdrCommCode = "";
+        string TypeOfHdrMsgSize = "";
+        string TypeOfMsgType = "";
+
+        UInt32 ValidCode = 0;
+
+        UInt32 RECV_BUFFER_LENGTH = 0;
+
+        string RpcCSContent = $@"
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+struct stMSG_HDR
+{{
+    public {TypeOfHdrCommCode} byCode;
+    public {TypeOfHdrMsgSize} bySize;
+    public {TypeOfMsgType} byType;
+}}
+
+public class RPC : MonoBehaviour
+{{
+    static RPC s_Instance;
+    public static RPC Instance {{ get {{ Init(); return s_Instance; }} }}
+    public static Proxy proxy = new Proxy();
+    public static {TypeOfHdrCommCode} ValidCode = {ValidCode};
+    private Dictionary<int, Action<byte[]>> StubMethods = new Dictionary<int, Action<byte[]>>();
+
+    NetworkManager m_NetworkManager = new NetworkManager();
+    public static NetworkManager Network {{ get {{ return Instance.m_NetworkManager; }} }}
+
+    private void Start()
+    {{
+        Init();
+    }}
+
+    private static void Init()
+    {{
+        GameObject go = GameObject.Find(""@RPC"");
+        if(go == null)
+        {{
+            go = new GameObject {{ name = ""@RPC"" }};
+            go.AddComponent<RPC>();
+
+            DontDestroyOnLoad(go);
+            s_Instance = go.GetComponent<RPC>();
+        }}
+    }}
+
+    public bool Initiate(string serverIP, UInt16 serverPort)
+    {{
+        return Network.Connect(serverIP, serverPort);
+    }}
+
+    public void AttachStub(Stub stub)
+    {{
+        foreach (var method in stub.methods)
+        {{
+            StubMethods.Add(method.Key, method.Value);
+        }}
+    }}
+
+    private void Update()
+    {{
+        while(Network.ReceivedDataSize() >= Marshal.SizeOf<stMSG_HDR>())
+        {{
+            stMSG_HDR hdr;
+            Network.Peek<stMSG_HDR>(out hdr);
+            if (Marshal.SizeOf<stMSG_HDR>() + hdr.bySize <= Network.ReceivedDataSize())
+            {{
+                Network.ReceiveData<stMSG_HDR>(out hdr);
+                byte[] payload = Network.ReceiveBytes(hdr.bySize);
+                if (StubMethods.ContainsKey(hdr.byType))
+                {{
+                    StubMethods[hdr.byType].Invoke(payload);
+                }}
+            }}
+            else
+            {{
+                break;
+            }}
+        }}
+    }}
+}}
+";
+
+        string NetMgrCSContent = $@"
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+
+public class NetBuffer
+{{
+    private byte[] m_Buffer;
+    private int m_Index;
+
+    public int BufferedSize {{ get {{ return m_Index; }} }}
+    public int FreeSize {{ get {{ return m_Buffer.Length - m_Index; }} }}
+
+    public NetBuffer(int buffSize)
+    {{
+        m_Buffer = new byte[buffSize];
+    }}
+
+    public bool Peek(byte[] dest, int length, int offset = 0)
+    {{
+        if(length + offset > m_Index)
+        {{
+            return false;
+        }}
+
+        Array.Copy(m_Buffer, offset, dest, 0, length);
+        return true;
+    }}
+
+    public bool Write(byte[] source, int length, int offset = 0)
+    {{
+        if (m_Index + length > m_Buffer.Length)
+        {{
+            return false;
+        }}
+
+        Array.Copy(source, offset, m_Buffer, m_Index, length);
+        m_Index += length;
+        return true;
+    }}
+    public bool WriteFront(byte[] source, int length, int offset = 0)
+    {{
+        if (m_Index + length > m_Buffer.Length)
+        {{
+            return false;
+        }}
+
+        if (m_Index == 0)
+        {{
+            Write(source, length, offset);
+        }}
+        else
+        {{
+            byte[] newBuffer = new byte[m_Buffer.Length];
+
+            Array.Copy(source, 0, newBuffer, 0, length);
+            Array.Copy(m_Buffer, 0, newBuffer, length, BufferedSize);
+            m_Index = BufferedSize + length;
+            m_Buffer = newBuffer;
+        }}
+
+        return true;
+    }}
+
+    public bool Read(byte[] dest, int length)
+    {{
+        if (m_Index < length)
+        {{
+            return false;
+        }}
+
+        Array.Copy(m_Buffer, dest, length);
+
+        if (m_Index == length)
+        {{
+            m_Index = 0;
+        }}
+        else
+        {{
+            byte[] newBuffer = new byte[m_Buffer.Length];
+            Array.Copy(m_Buffer, length, newBuffer, 0, BufferedSize - length);
+            m_Index = BufferedSize - length;
+            m_Buffer = newBuffer;
+        }}
+        return true;
+    }}
+}}
+
+public class NetworkManager
+{{
+    private TcpClient m_TcpClient = null;
+    private NetworkStream m_Stream = null;
+
+    private NetBuffer m_RecvBuffer = new NetBuffer({RECV_BUFFER_LENGTH});
+
+    private System.Random m_RandKeyMaker = new System.Random();
+
+    public NetworkManager(IPEndPoint ipEndPoint = null)
+    {{
+        if (ipEndPoint == null)
+        {{
+            m_TcpClient = new TcpClient();
+        }}
+        else
+        {{
+            m_TcpClient = new TcpClient(ipEndPoint);
+            m_Stream = m_TcpClient.GetStream();
+        }}
+    }}
+
+    public bool Connected {{ get {{ return m_TcpClient.Connected; }} }}
+
+    public bool Connect(string serverIP = ""127.0.0.1"", int port = 7777)
+    {{
+        if (!Connected)
+        {{
+            try
+            {{
+                m_TcpClient.Connect(IPAddress.Parse(serverIP), port);
+                m_Stream = m_TcpClient.GetStream();
+            }}
+            catch
+            {{
+                return false;
+            }}
+        }}
+
+        return true;
+    }}
+
+    public void Disconnect()
+    {{
+        if (Connected)
+        {{
+            m_TcpClient.Close();
+        }}
+    }}
+
+    public void ClearRecvBuffer()
+    {{
+        while (m_Stream.DataAvailable)
+        {{
+            byte[] buffer = new byte[1024];
+            m_Stream.Read(buffer, 0, buffer.Length);
+        }}
+    }}
+
+    public void SendBytes(byte[] data)
+    {{
+        m_Stream.Write(data);
+    }}
+
+    public bool ReceiveDataAvailable()
+    {{
+        return m_Stream.DataAvailable;
+    }}
+
+    public int ReceivedDataSize()
+    {{
+        return m_RecvBuffer.BufferedSize + m_TcpClient.Available;
+    }}
+
+    public bool Peek<T>(out T data)
+    {{
+        int dataSize = Marshal.SizeOf(typeof(T));
+        if (m_RecvBuffer.BufferedSize + m_TcpClient.Available < dataSize)
+        {{
+            data = default(T);
+            return false;
+        }}
+
+        if(m_RecvBuffer.BufferedSize < dataSize)
+        {{
+            int resSize = dataSize - m_RecvBuffer.BufferedSize; 
+            if(m_RecvBuffer.FreeSize < resSize)
+            {{
+                data = default(T);
+                return false;
+            }}
+
+            byte[] buffer = new byte[resSize];  
+            m_Stream.Read(buffer, 0, buffer.Length);
+            m_RecvBuffer.Write(buffer, resSize, 0); 
+        }}
+
+        byte[] bytes = new byte[dataSize];
+        m_RecvBuffer.Peek(bytes, bytes.Length, 0);
+        data = BytesToMessage<T>(bytes);
+
+        return true;
+    }}
+
+    public byte[] ReceiveBytes(int length)
+    {{
+        byte[] bytes = new byte[length];
+        if (m_RecvBuffer.BufferedSize + m_TcpClient.Available < length)
+        {{
+            return null;
+        }}
+
+        if(m_RecvBuffer.BufferedSize >= length)
+        {{
+            m_RecvBuffer.Read(bytes, length);
+        }}
+        else
+        {{
+            int buffedSize = m_RecvBuffer.BufferedSize;
+            m_RecvBuffer.Read(bytes, buffedSize);
+            m_Stream.Read(bytes, buffedSize, length - buffedSize);
+        }}
+        return bytes;
+    }}
+
+    public bool ReceiveData<T>(out T data)
+    {{
+        data = default(T);
+
+        if (ReceivedDataSize() < Marshal.SizeOf<T>())
+        {{
+            return false;
+        }}
+
+        byte[] receivedBytes = new byte[Marshal.SizeOf<T>()];
+        if(m_RecvBuffer.BufferedSize >= receivedBytes.Length)
+        {{
+            m_RecvBuffer.Read(receivedBytes, receivedBytes.Length);
+        }}
+        else
+        {{
+            int bufferedSize = m_RecvBuffer.BufferedSize;
+            m_RecvBuffer.Read(receivedBytes, bufferedSize);
+            m_Stream.Read(receivedBytes, bufferedSize, receivedBytes.Length - bufferedSize);
+        }}
+
+        data = BytesToMessage<T>(receivedBytes);    
+        return true;
+    }}
+
+    private byte[] MessageToBytes<T>(T str)
+    {{
+        int size = Marshal.SizeOf(str);
+        byte[] arr = new byte[size];
+        IntPtr ptr = Marshal.AllocHGlobal(size);
+        try
+        {{
+            Marshal.StructureToPtr(str, ptr, false);
+            Marshal.Copy(ptr, arr, 0, size);
+        }}
+        catch (Exception ex)
+        {{
+            Debugger.Break();
+        }}
+        finally
+        {{
+            // 할당받은 네이티브 메모리 해제
+            Marshal.FreeHGlobal(ptr);
+        }}
+
+        return arr;
+    }}
+
+    public void MessageToBytes<T>(T str, byte[] dest)
+    {{
+        int size = Marshal.SizeOf(str);
+
+        IntPtr ptr = Marshal.AllocHGlobal(size);
+        try
+        {{
+            Marshal.StructureToPtr(str, ptr, false);
+            Marshal.Copy(ptr, dest, 0, size);
+        }}
+        catch (Exception ex)
+        {{
+            Debugger.Break();
+        }}
+        finally
+        {{
+            // 할당받은 네이티브 메모리 해제
+            Marshal.FreeHGlobal(ptr);
+        }}
+    }}
+
+    public T BytesToMessage<T>(byte[] bytes)
+    {{
+        T str = default(T);
+        int size = Marshal.SizeOf<T>();
+        IntPtr ptr = Marshal.AllocHGlobal(size);
+
+        try
+        {{
+            Marshal.Copy(bytes, 0, ptr, size);
+            str = Marshal.PtrToStructure<T>(ptr);
+        }}
+        catch (Exception ex)
+        {{
+            Debugger.Break();
+        }}
+        finally
+        {{
+            Marshal.FreeHGlobal(ptr);
+        }}
+
+        return str;
+    }}
+}}
+";
+
+        using(FileStream fsRpc = OpenFile(commRpcPath))
+        {
+            using(StreamWriter sw = new StreamWriter(fsRpc))
+            {
+                sw.Write(RpcCSContent);
+            }
+        }
+        using (FileStream fsNet = OpenFile(commNetworkPath))
+        {
+            using(StreamWriter sw = new StreamWriter(fsNet))
+            {
+                sw.Write(NetMgrCSContent);
+            }
+        }
+    }
+
+    private void MakeProxy_Client(List<JPD_NAMESPACE> jpdNamespaces)
+    {
+        string proxyPath = m_JPD.CLIENT_OUTPUT_DIR + "\\Proxy.cs";
+        UInt32 ValidCode = 0;
+
+        string intro = $@"
+using System;
+using System.Runtime.InteropServices;
+
+public class Proxy
+{{
+";
+        string outro = $@"
+}}
+";
+
+        List<string> proxyFuncDefs = new List<string>();
+        foreach (var jpdNamespace in jpdNamespaces)
+        {
+            foreach(var jpdMessage in jpdNamespace.Defines)
+            {
+                string defStr = MakeClientProxyFunc(jpdMessage);
+                proxyFuncDefs.Add(defStr);
+            }
+        }
+
+        using (FileStream fs = OpenFile(proxyPath))
+        {
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                sw.Write(intro);
+
+                foreach (var proxyFuncDef in proxyFuncDefs)
+                {
+                    sw.Write(proxyFuncDef);
+                }
+
+                sw.Write(outro);
+            }
+        }
+    }
+
+    private string MakeClientProxyFunc(JPD_MESSAGE jpdMessage)
+    {
+        string TypeOfHdrMsgLen = "";
+        string parmeters = "";
+        string sizeofParamters = "";
+        for(int i=0; i<jpdMessage.Param.Count; i++)
+        {
+            parmeters += jpdMessage.Param[i].Type + " " + jpdMessage.Param[i].Name;
+            sizeofParamters += "Marshal.SizeOf(" + jpdMessage.Param[i].Name + ")";
+            if (i != jpdMessage.Param.Count - 1)
+            {
+                parmeters += ", ";
+                sizeofParamters += " + ";
+            }
+        }
+        string funcDef = $@"
+    public void {jpdMessage.Message}({parmeters})
+    {{
+        stMSG_HDR hdr = new stMSG_HDR();
+        hdr.Code = PRC.ValidCode;
+        hdr.MsgLen = ({TypeOfHdrMsgLen})({sizeofParamters});
+        hdr.MsgType = MessageIDs[""{jpdMessage.Message}""];
+
+        byte[] bytes = new byte[Marshal.Sizeof(hdr) + hdr.MsgLen];
+
+        byte[] bytesHdr = new byte[Marshal.SizeOf(hdr)];
+        RPC.Network.MessageToBytes<stMSG_HDR>(hdr, bytesHdr);
+
+        int offset = 0;
+";
+
+        foreach(var param in jpdMessage.Param)
+        {
+            string csharType = TranslateCSharpType(param.Type); 
+            if(csharType == "Byte")
+            {
+                funcDef += $@"
+            bytes[offset++] = {param.Name};
+";
+            }
+            else
+            {
+                funcDef += $@"
+            Buffer.BlockCopy(BitConverter.GetBytes({param.Name}), 0, bytes, offset, sizeof({csharType})); offset += sizeof({csharType});
+";
+            }
+        }
+
+        funcDef += $@"
+        RPC.Network.SendBytes(bytes);          
+    }}
+";
+
+        return funcDef;
+    }
+
+    string TranslateCSharpType(string type)
+    {
+        string ret = string.Empty;
+        switch (type)
+        {
+            case "BYTE":
+                ret = "Byte";
+                break;
+            case "CHAR":
+                ret = "Char";
+                break;
+            case "UINT16":
+                ret = "UInt16";
+                break;
+            case "INT16":
+                ret = "Int16";
+                break;
+            case "UINT32":
+                ret = "UInt32";
+                break;
+            case "INT32":
+                ret = "Int32";
+                break;
+            case "UINT64":
+                ret = "UInt64";
+                break;
+            case "INT64":
+                ret = "Int64";
+                break;
+            default:
+                break;
+        }
+
+        return ret;
     }
 
     private bool Compile_HDR_ONLY()
